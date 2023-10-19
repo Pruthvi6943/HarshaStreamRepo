a62378dd984e47bb579c7f3ea0d8cf81eb078f29
Harshii03 <139242152+Harshii03@users.noreply.github.com>
2023-10-19 09:57:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Harsha_files')
 
 #stream feature - @TeamHMT_Bots
 STREAM_BIN = environ.get("STREAM_BIN", "-1002132424439") #Log channel/Bin Channel id -100xxxxxxx
-STREAM_URL = environ.get("STREAM_URL", "https://harshafilestream-fc235c3a05ce.herokuapp.com") # https://example.com/ (Your Stream Url)
+STREAM_URL = environ.get("STREAM_URL", "harshafilestream-fc235c3a05ce.herokuapp.com") # https://example.com/ (Your Stream Url)
 IS_STREAM = bool(STREAM_BIN and STREAM_URL)
 #Use this feature, if you have deployed file to link bot. paste the app url with stream url and add filter bot as admin in bin channel and add id in stream bin
 
